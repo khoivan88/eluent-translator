37be7a7dae1f480485814707eb4451a548afeb1d
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-11-14 01:50:41 -0500

    Turn off service worker for now so contact form submission through netlify works

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,31 +3,16 @@ import SmoothScroll from 'smooth-scroll';
 
 
 // Check that service workers are supported
-if ('serviceWorker' in navigator) {
-  // use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    try {
-      navigator.serviceWorker.register('/service-worker.js');
-    } catch (error) {
-      console.error('Service worker registration failed: ', error);
-    }
-  });
-}
+//if ('serviceWorker' in navigator) {
+//  // use the window load event to keep the page load performant
+//  window.addEventListener('load', () => {
+//    try {
+//      navigator.serviceWorker.register('/service-worker.js');
+//    } catch (error) {
+//      console.error('Service worker registration failed: ', error);
+//    }
+//  });
+//}
 
 // Initialize Smooth scroll
 var scroll = new SmoothScroll('a[href*="#"]');
-
-// Contact form submission: https://docs.netlify.com/forms/setup/#submit-html-forms-with-ajax
-// const handleSubmit = (e) => {
-//   e.preventDefault();
-//   let myForm = document.getElementById('contactForm');
-//   let formData = new FormData(myForm);
-//   fetch('/', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-//     body: new URLSearchParams(formData).toString(),
-//   })
-//     .then(() => console.log('Form successfully submitted'))
-//     .catch((error) => alert(error));
-// };
-// document.querySelector('form').addEventListener('submit', handleSubmit);
