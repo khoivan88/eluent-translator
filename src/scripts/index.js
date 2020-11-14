3a70eb5fed484a9c631891f0d0c3e679de849e6d
Khoi Van <33493502+khoivan88@users.noreply.github.com>
2020-11-14 13:49:29 -0500

    Test for fixing netlify form submission giving page not found. What: - Turn on service worker - Add google-analytic as ignore host - Add `action='/'` as suggested by Stefan Frede

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,17 @@
 import 'alpinejs';
 import SmoothScroll from 'smooth-scroll';
 
-
 // Check that service workers are supported
-//if ('serviceWorker' in navigator) {
-//  // use the window load event to keep the page load performant
-//  window.addEventListener('load', () => {
-//    try {
-//      navigator.serviceWorker.register('/service-worker.js');
-//    } catch (error) {
-//      console.error('Service worker registration failed: ', error);
-//    }
-//  });
-//}
+if ('serviceWorker' in navigator) {
+  // use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    try {
+      navigator.serviceWorker.register('/service-worker.js');
+    } catch (error) {
+      console.error('Service worker registration failed: ', error);
+    }
+  });
+}
 
 // Initialize Smooth scroll
 var scroll = new SmoothScroll('a[href*="#"]');
